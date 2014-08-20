afc3d0669a6c299b8d51ae2188fcc56810d92f2f
coon <selikhovleonid@gmail.com>
2014-08-20 19:27:23 +0400

    Расширена документация phpDoc

--- core/IFrontController.php
+++ core/IFrontController.php
@@ -1,18 +1,31 @@
 <?php
 
 /**
- *
+ * Интерфейс описывает функциональность шаблона Front Controller.
  * @author coon
  */
 
 namespace core;
 
 interface IFrontController {
-	
+
+	/**
+	 * Запускает веб приложение на исполнение.
+	 * @return void.
+	 */
 	public static function run();
 
+	/**
+	 * Метод инициализирует настройки при первоначальном запуске приложения.
+	 * @return void.
+	 */
 	public function init();
 
+	/**
+	 * Обрабатывает объект Запроса, передавая его объекту Преобразователя 
+	 * контроллеров.
+	 * @return void.
+	 */
 	public function handleRequest();
 }
 
--- core/WebApp.php
+++ core/WebApp.php
@@ -1,8 +1,9 @@
 <?php
 
 /**
- * Description of WebApp
- *
+ * Класс определяет центральную точку входа для всех запросов, создает 
+ * конфигурированное веб приложение. 
+ * Реализует шаблон Front Controller, является Singleton-ом.
  * @author coon
  */
 
@@ -11,24 +12,40 @@ namespace core;
 class WebApp implements IFrontController {
 
 	private function __construct() {
+		// nothing here...
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public static function run() {
 		$oInstance = new self();
 		$oInstance->init();
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function init() {
 		$this->_initAutoload();
 		$this->handleRequest();
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function handleRequest() {
-		$oRequest = new Request();
-		$oCtrlResolver = new ControllerResolver($oRequest);
+		$oRequest		 = new Request();
+		$oCtrlResolver	 = new ControllerResolver($oRequest);
 		$oCtrlResolver->run();
 	}
-	
+
+	/**
+	 * Инициализация автоподгрузки всех классов приложения. Объект 
+	 * Автоподгрузчика получает все корни директорий из объекта 
+	 * Помощника приложения, после чего связывает их с автоподгрузкой. 
+	 * @return void.
+	 */
 	private function _initAutoload() {
 		$aRoot = AppHelper::getInstance()->getAutoloadRootSet();
 		foreach ($aRoot as $sRoot) {
@@ -36,5 +53,5 @@ class WebApp implements IFrontController {
 		}
 		\Autoloader::getInstance()->run();
 	}
-}
 
+}
