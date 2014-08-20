5a877d3733bc17075b768304111e818a0a9680e1
coon <selikhovleonid@gmail.com>
2014-08-20 15:28:57 +0400

    Определение режима отладки вынесено в индексный файл, добавлена phpDoc.

--- Autoloader.php
+++ Autoloader.php
@@ -1,26 +1,35 @@
 <?php
 
 /**
- * Description of Autoloader
+ * Автоподгрузчик классов PHP, реализован как Singleton.
  *
  * @author coon
  */
-
-if (DEBUG_APP) {
-	error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-}
-
 class Autoloader {
-	
+
 	const APP_ROOT = APP_ROOT;
+
+	/**
+	 * @var Autoloader 
+	 */
 	private static $_instance = NULL;
+
+	/**
+	 * @var array {
+	 * 		@type string $root Корень директории для подргузки
+	 * 		@type boolean $isLoaded Флаг, определяющий используется ли директория в автоподгрузке
+	 * } Множество корней директорий для автоподгрузки. 
+	 */
 	private static $_rootSet = array();
 
 	private function __construct() {
 		// nothing here
 	}
 
+	/**
+	 * Возвращает singleton-экземпляр класса Autoloder.
+	 * @return Autoloder
+	 */
 	public static function getInstance() {
 		if (is_null(self::$_instance)) {
 			self::$_instance = new self();
@@ -28,6 +37,11 @@ class Autoloader {
 		return self::$_instance;
 	}
 
+	/**
+	 * Проверка на вхождение во множество уже зарегестрированных корней для автоподгрузки.
+	 * @param string $sRoot Корень директории относительно корня приложения.
+	 * @return boolean
+	 */
 	private function _isInRootSet($sRoot) {
 		foreach (self::$_rootSet as $aSet) {
 			if ($aSet['root'] == $sRoot) {
@@ -37,6 +51,11 @@ class Autoloader {
 		return FALSE;
 	}
 
+	/**
+	 * Добавляет корень директории для автоподгрузки, выполняя проверку на дублирование.
+	 * @param string $sRoot Корень директории относительно корня приложения.
+	 * @return Autoloader
+	 */
 	public function add($sRoot) {
 		$sRoot = trim($sRoot);
 		if (substr($sRoot, -1) == DIRECTORY_SEPARATOR) {
@@ -53,7 +72,11 @@ class Autoloader {
 		}
 		return self::$_instance;
 	}
-	
+
+	/**
+	 * Аксессор, возвращающий множество корней директорий для автоподгрузки.
+	 * @return array Множество зарегистрированных корней.
+	 */
 	public function getRootSet() {
 		return self::$_rootSet;
 	}
@@ -67,8 +90,14 @@ class Autoloader {
 		}
 	}
 
+	/**
+	 * Возвращает безымянную замкнутую функцию автоподгрузки классов PHP
+	 * (как в стиле PEAR, так и основанную на неймспейсах).
+	 * @param string $sRoot
+	 * @return callable Замыкание (closure)
+	 */
 	private static function _getFuncCall($sRoot) {
-	// Currying
+		// Currying
 		return function ($sClassName) use ($sRoot) {
 					if (preg_match('/\\\\/', $sClassName)) {
 						$sRelativePath = str_replace('\\', DIRECTORY_SEPARATOR, $sClassName);
--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
+// Определение корня приложения как глобальной константы.
 defined('APP_ROOT') || define('APP_ROOT', __DIR__);
-defined('DEBUG_APP') || define('DEBUG_APP', TRUE);
+
+// Вкл/выкл режима отладки.
+defined('DEBUG_MODE') || define('DEBUG_MODE', TRUE);
+if (DEBUG_MODE) {
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+}
 
 require_once 'Autoloader.php';
 Autoloader::getInstance()
-		->add(DIRECTORY_SEPARATOR) // add core lib root
+		// Добавление корня пакета core в стек автоподгрузки.
+		// По умолчанию директория core находится в корне веб приложения.
+		->add(DIRECTORY_SEPARATOR)
 		->run();
+
+// Запуск веб приложения.
 \core\WebApp::run();
