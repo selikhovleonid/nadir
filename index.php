cbe48f816e7c2f3c25931f68c87d99ed88ce8206
coon <selikhovleonid@gmail.com>
2014-08-21 20:33:01 +0400

    Настройка PHP заменена на alias

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ defined('APP_ROOT') || define('APP_ROOT', __DIR__);
 // Вкл/выкл режима отладки.
 defined('DEBUG_MODE') || define('DEBUG_MODE', TRUE);
 if (DEBUG_MODE) {
-	error_reporting(E_ALL);
+	ini_set('error_reporting', E_ALL);
 	ini_set('display_errors', 1);
 }
 
 require_once 'Autoloader.php';
 Autoloader::getInstance()
-		// Добавление корня пакета core в стек автоподгрузки.
+		// Добавление корня пакета core в map автоподгрузки.
 		// По умолчанию директория core находится в корне веб приложения.
 		->add(DIRECTORY_SEPARATOR)
 		->run();
