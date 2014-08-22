2067900f912bf079750824caa554f7a64907649b
coon <selikhovleonid@gmail.com>
2014-08-22 19:39:44 +0400

    Добавлена phpDoc.

--- core/ControllerResolver.php
+++ core/ControllerResolver.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Description of ControllerResolver
- *
+ * Класс отвечает за выбор контроллера, передачу в него параметров 
+ * запроса, связывание с соответствующими макетом и представлением.
  * @author coon
  */
 
@@ -10,17 +10,35 @@ namespace core;
 
 class ControllerResolver {
 
-	private $_request	 = NULL;
-	private $_routeMap	 = array();
-	private $_ctrlName	 = '';
+	/** @var \core\Request Объект запроса. */
+	private $_request = NULL;
+
+	/** @var array[] Карта маршрутов. */
+	private $_routeMap = array();
+
+	/** @var string Имя контроллера. */
+	private $_ctrlName = '';
+
+	/** @var string Имя action. */
 	private $_actionName = '';
+
+	/** @var mixed[] Дополнительные параметры, передаваемые в action. */
 	private $_actionArgs = array();
 
+	/**
+	 * Инициализация приватных свойств объекта.
+	 * @return self.
+	 */
 	public function __construct(Request $oRequest) {
 		$this->_request	 = $oRequest;
 		$this->_routeMap = AppHelper::getInstance()->getConfig('routeMap');
 	}
 
+	/**
+	 * Создает объект контроллера, связывая его с умалчиваемыми объектами 
+	 * представления и макета.
+	 * @return \core\Controller.
+	 */
 	private function _createCtrl() {
 		$oView			 = ViewFactory::createView($this->_ctrlName, $this->_actionName);
 		$sCtrlNameFull	 = '\\controllers\\' . $this->_ctrlName;
@@ -28,7 +46,7 @@ class ControllerResolver {
 			$sLayoutName = AppHelper::getInstance()->getConfig('defaultLayout');
 			if (!is_null($sLayoutName)) {
 				$oLayout = ViewFactory::createLayout($sLayoutName, $oView);
-				$oCtrl = new $sCtrlNameFull($this->_request, $oLayout);
+				$oCtrl	 = new $sCtrlNameFull($this->_request, $oLayout);
 			} else {
 				$oCtrl = new $sCtrlNameFull($this->_request, $oView);
 			}
@@ -38,11 +56,21 @@ class ControllerResolver {
 		return $oCtrl;
 	}
 
+	/**
+	 * Рефлексивный вызов action контроллера с пердачей дополнительных параметров
+	 * запроса.
+	 * @param \core\AController $oCtrl
+	 */
 	private function _callActionWithArgs(AController $oCtrl) {
 		$oMethod = new \ReflectionMethod($oCtrl, $this->_actionName);
 		$oMethod->invokeArgs($oCtrl, $this->_actionArgs);
 	}
 
+	/**
+	 * Метод осуществляет попытку связать маршрут запроса с конкретным контроллером
+	 * и action в нем по карте регулярных выражений маршрутов.
+	 * @return void.
+	 */
 	private function _tryAssignController() {
 		foreach ($this->_routeMap as $sRoute => $aCtrlConf) {
 			if (preg_match('#^' . $sRoute . '/?$#', $this->_request->getUrlPath(), $aParam)) {
@@ -55,10 +83,19 @@ class ControllerResolver {
 		}
 	}
 
+	/**
+	 * Метод проверяет, осуществлена ли связь маршрута запроса с конкретным 
+	 * контроллером и action в нем.
+	 * @return boolean
+	 */
 	private function _isControllerAssigned() {
 		return !empty($this->_ctrlName) && !empty($this->_actionName);
 	}
 
+	/**
+	 * Запуск action контроллера на исполнение.
+	 * @throws CoreException.
+	 */
 	public function run() {
 		$this->_tryAssignController();
 		if ($this->_isControllerAssigned()) {
