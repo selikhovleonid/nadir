25b000b07aa4b9b5fd1c9beb57cbd66e96f1224f
coon <selikhovleonid@gmail.com>
2014-08-22 13:26:02 +0400

    Добавлена документация phpDoc.

--- core/Validator.php
+++ core/Validator.php
@@ -1,26 +1,44 @@
 <?php
 
 /**
- * Description of Validator
- *
+ * Класс-валидатор. Определяет, соответствуют ли данные некоторому шаблону.
  * @author coon
  */
+
 namespace core;
 
 class Validator {
-	
+
+	/** @var mixed[] Шаблон. */
 	private $_pattern = array();
+
+	/** @var array Массив ошибок. */
 	private $_errorList = array();
-	
+
+	/**
+	 * @param mixed[] $aPattern Шаблон.
+	 * @return self.
+	 */
 	public function __construct(array $aPattern) {
 		$this->_pattern = $aPattern;
 	}
 
+	/**
+	 * Метод проверяет соответствие данных шаблону.
+	 * @todo Реализовать логику.
+	 * @param mixed[] $aData Данные для валидации.
+	 * @return boolean.
+	 */
 	public function isValid(array $aData) {
 		// TODO business logic
 		return TRUE;
 	}
-	
+
+	/**
+	 * Возвращает список ошибок.
+	 * @todo Реализовать логику.
+	 * @return array.
+	 */
 	public function getErrors() {
 		return $this->_errorList;
 	}
