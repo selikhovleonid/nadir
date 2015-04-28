dbb090f2b625e21a4b2cfbac5a6a02e8a942fea5
coon <coon@deneb>
2015-04-28 11:55:10 +0300

    Изменена табуляция.

--- layouts/main.php
+++ layouts/main.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<meta charset="utf-8">
-		<title>Nadir Framework</title>
-	</head>
-	<body>
-		<?php $this->view->render(); ?>
-	</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Nadir Framework</title>
+    </head>
+    <body>
+        <?php $this->view->render(); ?>
+    </body>
 </html>
