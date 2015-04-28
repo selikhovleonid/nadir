b5a028df59f46889bee22e51320c44fa7ea63712
coon <coon@deneb>
2015-04-28 11:55:37 +0300

    Изменена табуляция.

--- views/test/default.php
+++ views/test/default.php
@@ -1,13 +1,13 @@
 <!-- ... -->
 <div>
-        <h1>User <?= $this->isUserOnline ? 'online':  'offline'; ?></h1>
-	<h1><?= $this->foo; ?></h1>
-	<?php if (is_array($this->bar) && !empty($this->bar)): ?>
-		<ul>
-		<?php foreach ($this->bar as $elem): ?>
-			<li><?= $elem; ?></li>
-		<?php endforeach; ?>
-		</ul>
-	<?php endif; ?>
+    <h1>User <?= $this->isUserOnline ? 'online' : 'offline'; ?></h1>
+    <h1><?= $this->foo; ?></h1>
+    <?php if (is_array($this->bar) && !empty($this->bar)): ?>
+        <ul>
+            <?php foreach ($this->bar as $elem): ?>
+                <li><?= $elem; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </div>
 <!-- ... -->
