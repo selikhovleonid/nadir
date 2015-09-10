5e65070dae1518eccc8114679c38d00179e30a04
coon <coon@alcor>
2015-09-10 21:58:56 +0300

    Теперь создается специфицированный объект web-приложения.

--- src/index.php
+++ src/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once './core/Autoloader.php';
+
 \core\Autoloader::getInstance()
         // Установка пути к корню приложения.
         ->setAppRoot(__DIR__)
@@ -9,8 +10,8 @@ require_once './core/Autoloader.php';
         ->add(DIRECTORY_SEPARATOR)
         ->run();
 
-// Запуск веб приложения.
-\core\App::getInstance()
+// Запуск веб-приложения.
+\core\WebApp::getInstance()
         // Определение относительного пути к файлу с основной конфигурацией 
         // приложения.
         ->setConfigFile('/config/main.php')
