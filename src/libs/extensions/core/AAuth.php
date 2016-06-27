5fab233adb1b930402609562dd31aa94b299c182
coon <coon@deneb>
2016-06-27 16:43:40 +0300

    The phpDoc was corrected.

--- src/libs/extensions/core/AAuth.php
+++ src/libs/extensions/core/AAuth.php
@@ -1,25 +1,23 @@
 <?php
 
-/**
- * Абстрактный класс авторизации.
- * @author coon
- */
-
 namespace extensions\core;
 
 use \core\IRunnable;
 
+/**
+ * This's the abstract auth class.
+ * @author coon
+ */
 abstract class AAuth implements IRunnable {
 
     /**
-     * Метод осуществляет проверку на валидность авторизации пользователя.
+     * The method checks if the user auth is valid.
      * @return boolean.
      */
     abstract public function isValid();
 
     /**
-     * Метод содержит функционал, вызываемый по ветке неудачного прохождения
-     * авторизации.
+     * The method contains the code those invokes if the auth was failed.
      */
     abstract public function onFail();
 }
