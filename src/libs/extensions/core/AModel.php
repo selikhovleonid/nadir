7956818357ccb8a276920e0501293cf310e63167
coon <coon@deneb>
2016-06-27 17:27:39 +0300

    The phpDoc was corrected.

--- src/libs/extensions/core/AModel.php
+++ src/libs/extensions/core/AModel.php
@@ -1,13 +1,12 @@
 <?php
 
+namespace extensions\core;
+
 /**
  * This's an abstract model class. For functionality demonstration only.
  * @todo Realize the interface and business logic.
  * @author coon.
  */
-
-namespace extensions\core;
-
 abstract class AModel {
     // Put your code here.
 }
