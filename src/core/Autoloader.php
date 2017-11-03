86118fa0619f267bfe471868a5437e2f935b7cfa
coon <selikhovleonid@ya.ru>
2017-11-03 18:59:36 +0300

    The code was formatted. Properties of the class were renamed and some editings were added to the phpDoc. Task #1 - The project codebase doesn't meet the PSR-1 and PSR-2 standarts.

--- src/core/Autoloader.php
+++ src/core/Autoloader.php
@@ -7,21 +7,22 @@ namespace core;
  * the naming system adopted in the PEAR packages and the namespace-based system.
  * @author coon.
  */
-class Autoloader {
-
+class Autoloader
+{
     /** @var string The path to the root of application. */
-    private $_appRoot = NULL;
+    private $appRoot = null;
 
     /** @var self The singleton object of current class. */
-    private static $_instance = NULL;
+    private static $instance = null;
 
     /** @var array[] The root set. */
-    private $_rootSet = array();
+    private $rootSet = array();
 
     /**
      * @ignore.
      */
-    private function __construct() {
+    private function __construct()
+    {
         // nothing here
     }
 
@@ -29,11 +30,12 @@ class Autoloader {
      * It returns the singleton instance of Autoloader class.
      * @return self.
      */
-    public static function getInstance() {
-        if (is_null(self::$_instance)) {
-            self::$_instance = new self();
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
     /**
@@ -42,13 +44,14 @@ class Autoloader {
      * @param string $sRoot The directory root relative to the application root.
      * @return boolean.
      */
-    private function _isInRootSet($sRoot) {
-        foreach ($this->_rootSet as $aSet) {
+    private function _isInRootSet($sRoot)
+    {
+        foreach ($this->rootSet as $aSet) {
             if ($aSet['root'] == $sRoot) {
-                return TRUE;
+                return true;
             }
         }
-        return FALSE;
+        return false;
     }
 
     /**
@@ -56,60 +59,66 @@ class Autoloader {
      * @param string $sAppRoot The path to the application root.
      * @return self.
      */
-    public function setAppRoot($sAppRoot) {
-        $this->_appRoot = $sAppRoot;
-        return self::$_instance;
+    public function setAppRoot($sAppRoot)
+    {
+        $this->appRoot = $sAppRoot;
+        return self::$instance;
     }
 
     /**
      * It returns the root of application.
      * @return string.
      */
-    public function getAppRoot() {
-        return $this->_appRoot;
+    public function getAppRoot()
+    {
+        return $this->appRoot;
     }
 
     /**
      * It returns TRUE if the application root already defined. 
      * @return boolean.
      */
-    public function isAppRootSet() {
-        return !empty($this->_appRoot);
+    public function isAppRootSet()
+    {
+        return !empty($this->appRoot);
     }
 
     /**
      * It adds the directory root for autoloading and checks for duplication.  
      * @param string $sRoot The directory root relative to the application root.
      * @return self.
-     * @throws Exception It throws if the application root wasn't define.
+     * @throws Exception It's throwed if the application root wasn't defined.
      */
-    public function add($sRoot) {
+    public function add($sRoot)
+    {
+        if (!$this->isAppRootSet()) {
+            throw new Exception("Application root isn't defined.");
+        }
         if ($this->isAppRootSet()) {
             $sRoot = trim($sRoot);
             if (substr($sRoot, -1) == DIRECTORY_SEPARATOR) {
                 $sRoot = substr($sRoot, 0, -1);
             }
-            if (strpos($sRoot, $this->getAppRoot()) === FALSE) {
-                $sRoot = $this->getAppRoot() . $sRoot;
+            if (strpos($sRoot, $this->getAppRoot()) === false) {
+                $sRoot = $this->getAppRoot().$sRoot;
             }
             if (!$this->_isInRootSet($sRoot)) {
-                $this->_rootSet[] = array(
-                    'root'     => $sRoot,
-                    'isLoaded' => FALSE
+                $this->rootSet[] = array(
+                    'root' => $sRoot,
+                    'isLoaded' => false
                 );
             }
-            return self::$_instance;
-        } else {
-            throw new Exception("Application root isn't define.");
+            return self::$instance;
         }
     }
 
     /**
      * This's the accessor for returning the set of directories root for autoloading.
-     * @return string[] Множество зарегистрированных корней.
+     * @return string[] The set of registered roots.
      */
-    public function getRootSet() {
-        return $this->_rootSet;
+    public function getRootSet()
+    {
+        return $this->rootSet;
     }
 
     /**
@@ -117,12 +126,13 @@ class Autoloader {
      * roots become enabled for loading.
      * @return void.
      */
-    public function run() {
-        foreach ($this->_rootSet as & $aSet) {
+    public function run()
+    {
+        foreach ($this->rootSet as & $aSet) {
             if (!$aSet['isLoaded']) {
-                spl_autoload_register(self::_getFuncCall($aSet['root']), TRUE,
-                        TRUE);
-                $aSet['isLoaded'] = TRUE;
+                spl_autoload_register(self::_getFuncCall($aSet['root']), true,
+                    true);
+                $aSet['isLoaded'] = true;
             }
         }
     }
@@ -134,23 +144,23 @@ class Autoloader {
      * @param string $sRoot The directory root relative to the application root.
      * @return callable The closure function.
      */
-    private static function _getFuncCall($sRoot) {
+    private static function _getFuncCall($sRoot)
+    {
         // Currying
         return function ($sClassName) use ($sRoot) {
             if (preg_match('/\\\\/', $sClassName)) {
                 $sRelativePath = str_replace('\\', DIRECTORY_SEPARATOR,
-                        $sClassName);
+                    $sClassName);
             } elseif (preg_match('/_/', $sClassName)) {
                 $sRelativePath = str_replace('_', DIRECTORY_SEPARATOR,
-                        $sClassName);
+                    $sClassName);
             } else {
                 $sRelativePath = $sClassName;
             }
-            $sPath = $sRoot . DIRECTORY_SEPARATOR . $sRelativePath . '.php';
+            $sPath = $sRoot.DIRECTORY_SEPARATOR.$sRelativePath.'.php';
             if (is_readable($sPath)) {
                 require_once $sPath;
             }
         };
     }
-
 }
