0ed40ba68c582f7d8d64191afe1d263f3294724d
coon <selikhovleonid@ya.ru>
2017-11-04 16:52:19 +0300

    The code in the process class was formatted. Task #1 - The project codebase doesn't meet the PSR-2 standart.

--- src/libs/extensions/core/Process.php
+++ src/libs/extensions/core/Process.php
@@ -9,15 +9,16 @@ use \core\ProcessInterface;
  * It also kills user's processes if it needed. It realized as singleton.
  * @author coon.
  */
-class Process implements ProcessInterface {
-
+class Process implements ProcessInterface
+{
     /** @var self This's singleton object of current class. */
-    private static $_instance = NULL;
+    private static $instance = null;
 
     /**
      * @ignore.
      */
-    private function __construct() {
+    private function __construct()
+    {
         // Nothing here...
     }
 
@@ -25,18 +26,20 @@ class Process implements ProcessInterface {
      * It returns the singleton-instance of current class.
      * @return self.
      */
-    public static function getInstance() {
-        if (is_null(self::$_instance)) {
-            self::$_instance = new self();
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
     /**
      * The method contains the realization of the custom configuration init.
      * @return void.
      */
-    public function run() {
+    public function run()
+    {
         // Put your code here;
     }
 
@@ -44,8 +47,8 @@ class Process implements ProcessInterface {
      * The method implements the killing of processes launched by the user.
      * @return void.
      */
-    public function stop() {
+    public function stop()
+    {
         // Put your code here;
     }
-
 }
