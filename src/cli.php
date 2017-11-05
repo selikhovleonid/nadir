307bc7f4b7a93f6e8f482dbe44adc8d1a6d21365
coon <selikhovleonid@ya.ru>
2017-11-05 17:40:54 +0300

    The cli.php script file was updated with new namespaces. Task #4 - Provide namespaces compatibility with UpperCamelCase format.

--- src/cli.php
+++ src/cli.php
@@ -1,20 +1,19 @@
 <?php
-
 # Add follow line to the  .htaccess to give ability to return cli file with httpd.
 # RedirectMatch 403 /cli.php$
 
-require_once './core/Autoloader.php';
+require_once './Core/Autoloader.php';
 
-\core\Autoloader::getInstance()
-        // Setting the path to the root of application.
-        ->setAppRoot(__DIR__)
-        // Adding the root of core package to the autoloading stack.
-        // By default the core directory is in the application root.
-        ->add(DIRECTORY_SEPARATOR)
-        ->run();
+\Nadir\Core\Autoloader::getInstance()
+    // Setting the path to the root of application.
+    ->setAppRoot(__DIR__)
+    // Adding the root of core package to the autoloading stack.
+    // By default the core directory is in the application root.
+    ->add(DIRECTORY_SEPARATOR)
+    ->run();
 
 // Running cli application.
-\core\CliApp::getInstance()
-        ->setConfigFile('/config/main.php')
-        ->run();
+\Nadir\Core\CliApp::getInstance()
+    ->setConfigFile('/config/main.php')
+    ->run();
 
