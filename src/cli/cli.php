d89ac27a2fd668e69747242849ed38e53e6a4a83
coon <selikhovleonid@ya.ru>
2017-11-08 22:23:42 +0300

    The index and command line front scripts were moved to the web and cli directories respectively. The autoloading configs were updated. The gitignore file was extended. Task #6 - Improve Nadir-based project template.

---
+++ src/cli/cli.php
@@ -0,0 +1,19 @@
+<?php
+# Add follow line to the  .htaccess to give ability to return cli file with httpd.
+# RedirectMatch 403 /cli.php$
+
+require_once '../core/Autoloader.php';
+
+nadir\core\Autoloader::getInstance()
+    // Setting the path to the root of application.
+    ->setAppRoot(__DIR__.DIRECTORY_SEPARATOR.'..')
+    // Adding the root of core package to the autoloading stack.
+    // By default the core directory is in the application root.
+    ->add(DIRECTORY_SEPARATOR)
+    ->run();
+
+// Running cli application.
+nadir\core\CliApp::getInstance()
+    ->setConfigFile('/config/main.php')
+    ->run();
+
